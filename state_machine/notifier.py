290a8b7be9384bc24a1ca18e8574846a9176b9bc
Lukas Mueller <mail@lukasmueller.de>
2025-07-29 16:29:13 +0200

    Change urls in state_machine

--- state_machine/notifier.py
+++ state_machine/notifier.py
@@ -1,12 +1,12 @@
 import requests
 from state_machine import ArmProgram, GazeProgram
 
-ROBOT_CONTROLLER_URL = "http://robot-controller.local/arm_program"
-GAZE_ANIMATION_URL   = "http://gaze-animation.local/gaze_program"
+ROBOT_CONTROLLER_URL = "http://0.0.0.0:3333/start"
+GAZE_ANIMATION_URL   = "http://0.0.0.0:2222/trigger"
 
 def notify_arm_program(prog: ArmProgram):
     try:
-        #requests.post(ROBOT_CONTROLLER_URL, json={"program": prog.value}, timeout=0.5)
+        requests.post(ROBOT_CONTROLLER_URL, json={"program": prog.value}, timeout=0.5)
         print("To Robot Controller:", "{'program':", prog.value, "}")
     except Exception as e:
         print("ERROR while sending data to robot controller: ", str(e))
@@ -14,7 +14,7 @@ def notify_arm_program(prog: ArmProgram):
 
 def notify_gaze_program(prog: GazeProgram):
     try:
-        #requests.post(GAZE_ANIMATION_URL, json={"program": prog.value}, timeout=0.5)
+        requests.post(GAZE_ANIMATION_URL, json={"program": prog.value}, timeout=0.5)
         print("To Gaze Animation:", "{'program':", prog.value, "}")
     except Exception as e:
         print("ERROR while sending data to gaze animation: ", str(e))
